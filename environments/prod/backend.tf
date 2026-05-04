3fe59eddabdc5781f881104b9a819b5ec54ce2ee
dewwripper <deww.ripper@gmail.com>
2026-05-04 11:16:41 +0000

    fix: update S3 bucket and DynamoDB table names for dev, prod, and qa environments

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -4,10 +4,10 @@
 
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"   # Replace with your bucket name
+    bucket         = "khanhcmlab-prod-state-bucket"   # Replace with your bucket name
     key            = "prod/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-state-lock"        # Replace with your table name
+    dynamodb_table = "khanhcmlab-prod-state-lock"        # Replace with your table name
   }
 }
--- environments/qa/backend.tf
+++ environments/qa/backend.tf
@@ -4,10 +4,10 @@
 
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"   # Replace with your bucket name
+    bucket         = "khanhcmlab-qa-state-bucket"   # Replace with your bucket name
     key            = "qa/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-state-lock"        # Replace with your table name
+    dynamodb_table = "khanhcmlab-qa-state-lock"        # Replace with your table name
   }
 }
